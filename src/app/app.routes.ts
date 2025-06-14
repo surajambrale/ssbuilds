f0b368060d1fd50e886b208433bd00358a318736
surajambrale <surajambrale9003@gmail.com>
2025-06-14 10:51:23 +0530

    made single page web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,5 @@ import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
-    {path:'services',component: ServicesComponent},
-    {path:'about',component:AboutComponent},
-    {path:'contact',component:ContactComponent}
+   
 ];
